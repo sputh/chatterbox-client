be988a4847a7e8e6788711f9d96e44a8d2279138
sputh <samantha.puth@gmail.com>
2014-08-04 17:00:40 -0700

    organized messages into table, adding refresh message functionality, and message removal

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,6 +3,21 @@ var app = function() {
 
   var init = function() {
     getMessages();
+    setInterval(function() {
+      getMessages();
+      cleaner();
+    }, 5000);
+  }
+
+  var cleaner = function() {
+    var n = $('.tbody').children();
+    if(n.length > 50) {
+      console.log("nooo!");
+      for(var i = 0; i < 50; i++) {
+        console.log($('tr').filter(":last"));
+        $('tr').filter(":last").remove();
+      }
+    }
   }
 
   var getMessages = function() {
@@ -20,28 +35,29 @@ var app = function() {
   }
 
   Message.prototype.printHTML = function() {
-    var string = "<div>" + this.username + "@ " + this.room + ": " + this.text + "</div>";
+    var string = "<tr><td>" + this.username + "</td><td>@ " + this.room + "</td><td>" + this.text + "</td></tr>";
     return string;
   }
 
   var filter = function(data) {
   //return checker.test(data);
   return /\<|\>|\(.*\)|\=|TEST/.test(data) || (data === "") || (data === undefined);
-  }
+}
 
-  var displayMessage = function(data) {
-    var messages = data.results, msg;
-    console.log(messages);
+var displayMessage = function(data) {
+  var messages = data.results, msg;
+    // console.log(messages);
     for(var i = 0; i < messages.length; i++) {
       msg = new Message(messages[i]);
-      console.log(msg.text);
-      console.log(filter(msg.text));
+      // console.log(msg.text);
+      // console.log(filter(msg.text));
       if(!filter(msg.text) && !filter(msg.username)) {
-        $("body").append(msg.printHTML());
+        $(".tbody").append(msg.printHTML());
         //console.log(msg);
       }
     };
   }
+
   return {
     init : init
   };
